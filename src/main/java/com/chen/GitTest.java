631cfb94d3322f6a104927ba9f5f3269e97f5346
chenchenchen <2811908526@qq.com>
2022-07-29 23:52:52 +0800

    first commit

---
+++ src/main/java/com/chen/GitTest.java
@@ -0,0 +1,14 @@
+package com.chen;
+
+import org.junit.Test;
+
+/**
+ * @author chenchenchen
+ * @create 2022-07-29 23:50
+ */
+public class GitTest {
+    @Test
+    public void test1(){
+        System.out.println("hello git");
+    }
+}
