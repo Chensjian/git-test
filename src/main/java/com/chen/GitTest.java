4023583fbe03c598aaf590fdb58aa071838ce565
chenchenchen <2811908526@qq.com>
2022-07-29 23:55:55 +0800

    first commit

--- src/main/java/com/chen/GitTest.java
+++ src/main/java/com/chen/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
     public void test1(){
         System.out.println("hello git");
         System.out.println("hello git1");
+        System.out.println("hello git2");
     }
 }
